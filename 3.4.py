de7c0e2409fdfcbb1c35d41e1e15be97fde31ef6
Viooooletta <violeta.karavaj@gmail.com>
2023-10-25 01:48:03 +0300

    Это файл к заданию 3.3

---
+++ 3.4.py
@@ -0,0 +1,25 @@
+import json
+
+file = open('data/Данные о фирме.txt', 'r', encoding='utf-8')
+
+lines = file.readlines()
+
+profits = {}
+
+TotalProfit = 0
+count = 0
+for line in lines:
+    name, OwnerShip, revenue, costs = line.split()
+    revenue = int(revenue)
+    costs = int(costs)
+    profit = revenue - costs
+    profits[name] = profit
+    if profit > 0:
+        TotalProfit += profit
+        count += 1
+result = [profits, {'AvarageProfit': TotalProfit / count}]
+
+file.close()
+
+file = open('data/Данные о фирме.txt', 'w', encoding='utf-8')
+json.dump(result, file)
