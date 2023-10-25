9f52a258214beb302f022a45996d2c2f595fe20f
Viooooletta <violeta.karavaj@gmail.com>
2023-10-25 12:42:45 +0300

    Это файл к заданию 3.4

--- 3.4.py
+++ 3.4.py
@@ -9,17 +9,17 @@ profits = {}
 TotalProfit = 0
 count = 0
 for line in lines:
-    name, OwnerShip, revenue, costs = line.split()
-    revenue = int(revenue)
-    costs = int(costs)
-    profit = revenue - costs
-    profits[name] = profit
-    if profit > 0:
-        TotalProfit += profit
-        count += 1
-result = [profits, {'AvarageProfit': TotalProfit / count}]
-
+    values = line.split()
+    if len(values) == 4:
+        name, ownership, revenue, costs = values
+        profits = int(revenue) - int(costs)
+        profits[name] = profit
+        if profit > 0:
+            TotalProfit += profit
+            count += 1
+AvarageProfit = TotalProfit / count if count > 0 else 0
 file.close()
 
 file = open('data/Данные о фирме.txt', 'w', encoding='utf-8')
-json.dump(result, file)
+json.dump(AvarageProfit, file)
+file.close()
